d61e42344c6ac552132f7bd40ec95954071f2174
ArtemToporkov <toporkovartem259@gmail.com>
2025-10-11 10:45:11 +0500

    map /offer/:id to a specific offer

--- src/pages/offer-page/offer-page.tsx
+++ src/pages/offer-page/offer-page.tsx
@@ -1,4 +1,26 @@
-﻿export function OfferPage(): JSX.Element {
+﻿
+import { PremiumLabel } from '../../components/premium-label/premium-label.tsx';
+import { Amenity } from '../../enums/amenity.ts';
+import { useParams } from 'react-router-dom';
+import { offers } from '../../mocks/offers.ts';
+import { NotFoundPage } from '../not-found-page/not-found-page.tsx';
+
+export function OfferPage(): JSX.Element {
+  const { id } = useParams<{ id: string }>();
+  const offer = offers.find((of) => of.id === id);
+  if (offer === undefined) {
+    return <NotFoundPage />;
+  }
+  const {
+    description,
+    rating,
+    isPremium,
+    bedroomsCount,
+    maxAdultsCount,
+    hotelType,
+    price,
+    amenities
+  } = offer;
   return (
     <div className="page">
       <header className="header">
@@ -89,12 +111,10 @@
           </div>
           <div className="offer__container container">
             <div className="offer__wrapper">
-              <div className="offer__mark">
-                <span>Premium</span>
-              </div>
+              {isPremium && <PremiumLabel />}
               <div className="offer__name-wrapper">
                 <h1 className="offer__name">
-                  Beautiful &amp; luxurious studio at great location
+                  {description}
                 </h1>
                 <button className="offer__bookmark-button button" type="button">
                   <svg className="offer__bookmark-icon" width={31} height={33}>
@@ -108,34 +128,29 @@
                   <span style={{ width: '80%' }} />
                   <span className="visually-hidden">Rating</span>
                 </div>
-                <span className="offer__rating-value rating__value">4.8</span>
+                <span className="offer__rating-value rating__value">{rating}</span>
               </div>
               <ul className="offer__features">
-                <li className="offer__feature offer__feature--entire">Apartment</li>
+                <li className="offer__feature offer__feature--entire">{hotelType}</li>
                 <li className="offer__feature offer__feature--bedrooms">
-                  3 Bedrooms
+                  {bedroomsCount} Bedrooms
                 </li>
                 <li className="offer__feature offer__feature--adults">
-                  Max 4 adults
+                  Max {maxAdultsCount} adults
                 </li>
               </ul>
               <div className="offer__price">
-                <b className="offer__price-value">€120</b>
+                <b className="offer__price-value">€{price}</b>
                 <span className="offer__price-text">&nbsp;night</span>
               </div>
               <div className="offer__inside">
                 <h2 className="offer__inside-title">What&apos;s inside</h2>
                 <ul className="offer__inside-list">
-                  <li className="offer__inside-item">Wi-Fi</li>
-                  <li className="offer__inside-item">Washing machine</li>
-                  <li className="offer__inside-item">Towels</li>
-                  <li className="offer__inside-item">Heating</li>
-                  <li className="offer__inside-item">Coffee machine</li>
-                  <li className="offer__inside-item">Baby seat</li>
-                  <li className="offer__inside-item">Kitchen</li>
-                  <li className="offer__inside-item">Dishwasher</li>
-                  <li className="offer__inside-item">Cabel TV</li>
-                  <li className="offer__inside-item">Fridge</li>
+                  {(Object.keys(amenities) as Amenity[])
+                    .filter((am) => amenities[am])
+                    .map((am) => (
+                      <li key={am} className="offer__inside-item">{am}</li>
+                    ))}
                 </ul>
               </div>
               <div className="offer__host">
