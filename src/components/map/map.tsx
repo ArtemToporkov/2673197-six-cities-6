92f3966f090937925f503569b355667baa67c815
ArtemToporkov <toporkovartem259@gmail.com>
2025-10-29 19:25:51 +0500

    feat: add map component

---
+++ src/components/map/map.tsx
@@ -0,0 +1,56 @@
+﻿import { useRef, useEffect, ReactNode } from 'react';
+import { Icon, Marker, layerGroup } from 'leaflet';
+import { useMap } from '../../hooks/use-map';
+import { City } from '../../types/city';
+import 'leaflet/dist/leaflet.css';
+import { Point } from '../../types/point.ts';
+
+type MapProps = {
+  city: City;
+  points: Point[];
+  selectedPoint: Point | undefined;
+};
+
+const defaultCustomIcon = new Icon({
+  iconUrl: 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+const currentCustomIcon = new Icon({
+  iconUrl: 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+export function Map({city, points, selectedPoint}: MapProps): ReactNode {
+
+  const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      const markerLayer = layerGroup().addTo(map);
+      points.forEach((location) => {
+        const marker = new Marker({
+          lat: location.latitude,
+          lng: location.longitude
+        });
+
+        marker
+          .setIcon(
+            selectedPoint !== undefined && location.key === selectedPoint.key
+              ? currentCustomIcon
+              : defaultCustomIcon
+          )
+          .addTo(markerLayer);
+      });
+
+      return () => {
+        map.removeLayer(markerLayer);
+      };
+    }
+  }, [map, points, selectedPoint]);
+
+  return <div style={{height: '500px'}} ref={mapRef}></div>;
+}
---
+++ src/types/point.ts
@@ -0,0 +1,5 @@
+﻿export type Point = {
+  key: string;
+  latitude: number;
+  longitude: number;
+}
