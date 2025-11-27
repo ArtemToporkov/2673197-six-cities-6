f6f4f43f030635c35771610a645012956360a8ca
ArtemToporkov <toporkovartem259@gmail.com>
2025-11-27 14:09:52 +0500

    fix: switch map view after switching city

--- src/hooks/use-map.tsx
+++ src/hooks/use-map.tsx
@@ -12,6 +12,18 @@ export function useMap(
   const isRenderedRef = useRef<boolean>(false);
 
   useEffect(() => {
+    if (map && city) {
+      map.setView(
+        {
+          lat: city.location.latitude,
+          lng: city.location.longitude
+        },
+        city.location.zoom
+      );
+    }
+  }, [map, city]);
+
+  useEffect(() => {
     if (mapRef.current !== null && !isRenderedRef.current) {
       const instance = city
         ? new Map(mapRef.current, {
