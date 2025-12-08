ca7c88fd1fbe4de8b5ba447350f619b73643aea2
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-08 21:51:17 +0500

    feat: handle bookmark button click on offer page

--- src/components/bookmark-button/bookmark-button.tsx
+++ src/components/bookmark-button/bookmark-button.tsx
@@ -4,9 +4,11 @@ import { ReactNode, useState } from 'react';
 type BookmarkButtonProps = {
   active: boolean;
   onClick: () => void;
+  width?: number;
+  height?: number;
 };
 
-export function BookmarkButton({ active, onClick }: BookmarkButtonProps): ReactNode {
+export function BookmarkButton({ active, onClick, width, height }: BookmarkButtonProps): ReactNode {
   const [isActive, setIsActive] = useState<boolean>(active);
   return (
     <button
@@ -20,7 +22,7 @@ export function BookmarkButton({ active, onClick }: BookmarkButtonProps): ReactN
         setIsActive(!isActive);
       }}
     >
-      <svg className="place-card__bookmark-icon" width="18" height="19">
+      <svg className="place-card__bookmark-icon" width={width ?? 18} height={height ?? 19}>
         <use xlinkHref="#icon-bookmark"></use>
       </svg>
       <span className="visually-hidden">{isActive ? 'In' : 'To'} bookmarks</span>
