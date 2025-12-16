4c24413c279f2debe949ae79e6dd82703d2b8db6
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-16 21:34:25 +0500

    test: add user slice reducer tests

---
+++ src/store/user/user-slice.test.ts
@@ -0,0 +1,31 @@
+﻿import { describe, it, expect } from 'vitest';
+import { internet, image, datatype, random } from 'faker';
+
+import { changeUserInfo, userSlice } from './user-slice.ts';
+import { AuthStatus } from '../../enums/auth-status.ts';
+import type { User } from '../../types/user.ts';
+
+describe('reducer', () => {
+  it('should change user info', () => {
+    const initialState: User = {
+      authStatus: AuthStatus.Unknown,
+      info: null
+    };
+    const makeToken = () =>
+      `${random.alphaNumeric(10)}.${random.alphaNumeric(10)}.${random.alphaNumeric(10)}`;
+    const expectedState: User = {
+      authStatus: AuthStatus.Authorized,
+      info: {
+        name: internet.userName(),
+        avatarUrl: image.avatar(),
+        isPro: datatype.boolean(),
+        email: internet.email(),
+        token: makeToken()
+      }
+    };
+
+    const result = userSlice.reducer(initialState, changeUserInfo(expectedState));
+
+    expect(result).toEqual(expectedState);
+  });
+});
