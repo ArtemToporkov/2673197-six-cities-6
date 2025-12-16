97e5911d85980c8f46691c073bac07e3ab60ae25
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-16 21:18:39 +0500

    test: add tests for cities slice reducer

---
+++ src/store/cities/cities-slice.test.ts
@@ -0,0 +1,27 @@
+﻿import { describe, it, expect } from 'vitest';
+
+import { citiesSlice, loadCities, switchCity } from './cities-slice.ts';
+import { makeCity } from '../utils.ts';
+
+describe('Cities slice', () => {
+  const initialState = {
+    city: null,
+    cities: []
+  };
+
+  it('should load cities', () => {
+    const expectedCities = [makeCity(), makeCity()];
+
+    const result = citiesSlice.reducer(initialState, loadCities(expectedCities));
+
+    expect(result.cities).toEqual(expectedCities);
+  });
+
+  it('should switch city', () => {
+    const expectedCity = makeCity();
+
+    const result = citiesSlice.reducer(initialState, switchCity(expectedCity));
+
+    expect(result.city).toEqual(expectedCity);
+  });
+});
