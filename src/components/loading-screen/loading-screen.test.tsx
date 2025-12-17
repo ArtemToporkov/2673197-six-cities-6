bb71f5e14393294d9fec730e9648300a3bea4683
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-17 12:44:24 +0500

    test: add rendering tests for loading screen

---
+++ src/components/loading-screen/loading-screen.test.tsx
@@ -0,0 +1,14 @@
+﻿import { render, screen } from '@testing-library/react';
+
+import { LoadingScreen } from './loading-screen.tsx';
+
+describe('Component: Loading screen', () => {
+  describe('rendering', () => {
+    it('should display loading screen', () => {
+      render(<LoadingScreen />);
+
+      const loadingScreen = screen.getByRole('status', { 'name': /loading/i });
+      expect(loadingScreen).toBeInTheDocument();
+    });
+  });
+});
--- src/components/loading-screen/loading-screen.tsx
+++ src/components/loading-screen/loading-screen.tsx
@@ -4,7 +4,7 @@ import styles from './loading-screen.module.css';
 
 export function LoadingScreen(): ReactNode {
   return (
-    <div className={styles['loading-screen']}>
+    <div className={styles['loading-screen']} role='status' aria-label='loading'>
       <div className={styles['spinner']}></div>
     </div>
   );
