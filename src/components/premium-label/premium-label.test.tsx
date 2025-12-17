574d91d1bbbcad0ec1b47ee963802c22371b89db
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-17 12:44:45 +0500

    test: add rendering test for premium label

---
+++ src/components/premium-label/premium-label.test.tsx
@@ -0,0 +1,14 @@
+﻿import { render, screen } from '@testing-library/react';
+
+import { PremiumLabel } from './premium-label.tsx';
+
+describe('Component: Premium label', () => {
+  describe('rendering', () => {
+    it('should display "Premium" text', () => {
+      render(<PremiumLabel />);
+
+      const premiumText = screen.getByText(/premium/i);
+      expect(premiumText).toBeInTheDocument();
+    });
+  });
+});
