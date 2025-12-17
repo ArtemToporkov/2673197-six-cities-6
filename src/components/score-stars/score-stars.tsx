12efac3521541c1d7a51ccab2bba1b7325104dbc
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-17 12:45:01 +0500

    test: add rendering tests for score stars

--- src/components/score-stars/score-stars.tsx
+++ src/components/score-stars/score-stars.tsx
@@ -1,28 +1,17 @@
 ﻿import { Fragment } from 'react';
 import type { ReactNode } from 'react';
 
+import { RATINGS } from '../../const.ts';
 import type { RatingScore } from '../../types/rating-score.ts';
-
-type RatingWithDescription = {
-  score: RatingScore;
-  description: string;
-}
 
 type ScoreStarsProps = {
   onScoreChanged: (score: RatingScore) => void;
 }
 
 export function ScoreStars({ onScoreChanged }: ScoreStarsProps): ReactNode {
-  const ratings = [
-    {score: 5, description: 'perfect'},
-    {score: 4, description: 'good'},
-    {score: 3, description: 'not bad'},
-    {score: 2, description: 'badly'},
-    {score: 1, description: 'terribly'},
-  ] as RatingWithDescription[];
   return (
     <div className="reviews__rating-form form__rating">
-      {ratings.map(({score, description}) => (
+      {RATINGS.map(({score, description}) => (
         <Fragment key={score}>
           <input
             className="form__rating-input visually-hidden"
@@ -35,6 +24,7 @@ export function ScoreStars({ onScoreChanged }: ScoreStarsProps): ReactNode {
             htmlFor={`${score}-stars`}
             className="reviews__rating-label form__rating-label"
             title={description}
+            aria-label={description}
           >
             <svg className="form__star-image" width="37" height="33" onClick={() => onScoreChanged(score)}>
               <use xlinkHref="#icon-star"></use>
--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,18 @@
-﻿export const AUTH_TOKEN_KEY_NAME = 'six-cities-token';
+﻿import type { RatingScore } from './types/rating-score.ts';
+
+export const AUTH_TOKEN_KEY_NAME = 'six-cities-token';
+
 export const AUTH_HEADER_NAME = 'X-Token';
+
+type RatingWithDescription = {
+  score: RatingScore;
+  description: string;
+}
+
+export const RATINGS: RatingWithDescription[] = [
+  { score: 5, description: 'perfect' },
+  { score: 4, description: 'good' },
+  { score: 3, description: 'not bad' },
+  { score: 2, description: 'badly' },
+  { score: 1, description: 'terribly' },
+];
