6c4f4ff56b11910d3c47093d99945c24bf2d2bb2
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-17 14:45:22 +0500

    test: fix rendering tests for host card
    Removed incorrect object mutation in beforeEach

--- src/components/host-card/host-card.test.tsx
+++ src/components/host-card/host-card.test.tsx
@@ -1,42 +1,42 @@
 ﻿import { render, screen } from '@testing-library/react';
 import { datatype, image, internet } from 'faker';
 
-import type { HostInfo } from '../../types/host-info.ts';
 import { HostCard } from './host-card.tsx';
+import type { HostInfo } from '../../types/host-info.ts';
 
 describe('Component: Host card', () => {
   describe('rendering', () => {
-    let hostInfo: HostInfo;
-
-    beforeEach(() => {
-      hostInfo = {
-        avatarUrl: image.avatar(),
-        isPro: datatype.boolean(),
-        name: internet.userName()
-      };
-
-      render(<HostCard hostInfo={hostInfo} />);
+    const makeHostInfo = (initial?: Partial<HostInfo>): HostInfo => ({
+      avatarUrl: image.avatar(),
+      isPro: datatype.boolean(),
+      name: internet.userName(),
+      ...initial
     });
 
     it('should display host avatar', () => {
+      render(<HostCard hostInfo={makeHostInfo()} />);
+
       const avatar = screen.getByAltText(/host avatar/i);
       expect(avatar).toBeInTheDocument();
     });
 
     it('should display host name', () => {
+      const hostInfo = makeHostInfo();
+      render(<HostCard hostInfo={hostInfo} />);
+
       const name = screen.getByText(hostInfo.name);
       expect(name).toBeInTheDocument();
     });
 
     it('should display pro status when host\'s status is pro', () => {
-      hostInfo.isPro = true;
+      render(<HostCard hostInfo={makeHostInfo({ isPro: true })} />);
 
       const proText = screen.getByText(/pro/i);
       expect(proText).toBeInTheDocument();
     });
 
     it('should not display pro status when host\'s status is not pro', () => {
-      hostInfo.isPro = false;
+      render(<HostCard hostInfo={makeHostInfo({ isPro: false })} />);
 
       const proText = screen.queryByText(/pro/i);
       expect(proText).not.toBeInTheDocument();
