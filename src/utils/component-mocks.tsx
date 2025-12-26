695dc725831ac61dcc9097885439c382ea32179c
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-26 22:26:13 +0500

    feat: add util that adds mock store to component

---
+++ src/utils/component-mocks.tsx
@@ -0,0 +1,32 @@
+import MockAdapter from 'axios-mock-adapter';
+import type { ReactNode } from 'react';
+import { configureMockStore, MockStore } from '@jedmao/redux-mock-store';
+import thunk, { ThunkDispatch } from 'redux-thunk';
+import { Action } from 'redux';
+import { Provider } from 'react-redux';
+
+import { createApi } from '../services/api.ts';
+import type { State } from '../types/state.ts';
+
+type ComponentWithStore = {
+  withStoreComponent: ReactNode;
+  mockStore: MockStore;
+  mockAxiosAdapter: MockAdapter;
+}
+
+export function withStore(component: ReactNode, initialState: Partial<State>): ComponentWithStore {
+  const axios = createApi();
+  const mockAxiosAdapter = new MockAdapter(axios);
+  const middleware = [thunk.withExtraArgument(axios)];
+  const mockStoreCreator = configureMockStore<
+    State,
+    Action<string>,
+    ThunkDispatch<State, ReturnType<typeof createApi>, Action>
+  >(middleware);
+  const mockStore = mockStoreCreator(initialState);
+  return ({
+    withStoreComponent: <Provider store={mockStore}>{component}</Provider>,
+    mockStore,
+    mockAxiosAdapter
+  });
+}
