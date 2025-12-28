52ee6949da4302f9766e6901755a9cc5d6f333b6
ArtemToporkov <toporkovartem259@gmail.com>
2025-12-28 14:36:02 +0500

    feat: show first 10 sorted comments

--- src/components/offer-reviews/offer-reviews.tsx
+++ src/components/offer-reviews/offer-reviews.tsx
@@ -1,4 +1,4 @@
-﻿import type { ReactNode } from 'react';
+﻿import { useMemo, type ReactNode } from 'react';
 
 import { CommentsList } from '../comments-list/comments-list.tsx';
 import { CommentForm } from '../comment-form/comment-form.tsx';
@@ -13,12 +13,18 @@ type OfferReviewsProps = {
 export function OfferReviews({ comments }: OfferReviewsProps): ReactNode {
   const authStatus = useAppSelector((state) => state.user.authStatus);
 
+  const displayedComments = useMemo(() =>
+    [...comments]
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+      .slice(0, 10),
+  [comments]);
+
   return (
     <section className="offer__reviews reviews">
       <h2 className="reviews__title">
         Reviews · <span className="reviews__amount">{comments.length}</span>
       </h2>
-      <CommentsList comments={comments} />
+      <CommentsList comments={displayedComments} />
       {authStatus === AuthStatus.Authorized && <CommentForm />}
     </section>
   );
